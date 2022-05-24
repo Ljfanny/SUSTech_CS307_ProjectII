94d75d7b48600bfa7671ea83dc5efa8090e6c525
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2022-05-24 14:51:46 +0800

    hahahha

--- projectii/src/test/java/com/database/projectii/PressureTest.java
+++ projectii/src/test/java/com/database/projectii/PressureTest.java
@@ -2,6 +2,7 @@ package com.database.projectii;
 
 import com.database.projectii.model.Staff;
 import com.database.projectii.service.impl.StaffServiceImpl;
+import java.util.Random;
 import org.databene.contiperf.PerfTest;
 import org.databene.contiperf.Required;
 import org.databene.contiperf.junit.ContiPerfRule;
@@ -37,7 +38,8 @@ public class PressureTest {
     @PerfTest(invocations = 10000, threads = 100000)
     @Required(max = 100000, average = 20000, totalTime = 6000000)
     public void test() {
-        Staff staff = new Staff(1, null, null, null, null, null, null, null);
+        Random random = new Random();
+        Staff staff = new Staff(random.nextInt(999) + 1, null, null, null, null, null, null, null);
         System.out.println(staffServiceimpl.selectStaffByAny(staff));
     }
 }
